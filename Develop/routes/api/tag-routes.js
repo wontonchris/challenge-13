4f485ee097fd24f198a4c067a01d43e4d852e5ad
wontonchris <127700194+wontonchris@users.noreply.github.com>
2023-06-21 19:12:26 -0500

    debugged working on products

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({ include: [{ model: Product }] }).then((tags) => {
+    res.json(tags);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+Tag.findByPk(req.params.id, { include: [{ model: Product }] }).then((tag) => {
+  res.json(tag);
+});
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then((tag) => {
+    res.json(tag);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, { where: { id: req.params.id } }).then((tag) => {
+    res.json(tag);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } }).then((tag) => {
+    res.json(tag);
+  })
 });
 
 module.exports = router;
